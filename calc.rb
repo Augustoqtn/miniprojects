5a1eb55159bda2c1af0697748aea0c58e0f97922
Augusto <augustinhobrito542@gmail.com>
2021-06-01 15:12:41 -0300

    calculadora de soma

---
+++ calc.rb
@@ -0,0 +1,6 @@
+print "digite o primeiro numero inteiro por gentileza:"
+n1 = gets.chomp.to_i
+print "digite o segundo numero inteiro por gentileza:"
+n2 = gets.chomp.to_i
+addition = n1 + n2 
+puts "a soma destes dois numero é #{addition}"
